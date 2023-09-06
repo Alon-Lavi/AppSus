641bf4add7c8854c0bf32b955dcafa8fa0e66119
Daniel Kirushin <retrostyae@mail.ru>
2023-09-06 18:29:55 +0300

    New Commit Mail

--- apps/mail/cmps/MailBoxFilter.jsx
+++ apps/mail/cmps/MailBoxFilter.jsx
@@ -6,18 +6,8 @@ const { useState, useEffect, useRef } = React;
 export function MailBoxFilter ({ filterBy, onSetFilter }){
     const [filterByToEdit, setFilterByToEdit] = useState(filterBy);
 
-    useEffect(() => {
-        function handleClickOutside(event) {
-          if (menuRef.current && !menuRef.current.contains(event.target)) {
-            setIsMenuOpen(false);
-          }
-        }
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-          document.removeEventListener('mousedown', handleClickOutside);
-        };
-      }, [menuRef]);
 
+    
     return (
         <section>
           <button className="menu-button" onClick={() => setIsMenuOpen((prevState) => !prevState)}>
