dabb1179beec611dff5835bd3848ee274502a962
Daniel Kirushin <retrostyae@mail.ru>
2023-09-08 18:46:41 +0300

    Css/Mq/About

--- views/About.jsx
+++ views/About.jsx
@@ -1,6 +1,39 @@
 
 export function About() {
-    return <section className="about">
-        <h1>About Page</h1>
-    </section>
-}
+    const teamMembers = [
+        {
+            name: "Daniel Kirushin",
+            // image: "./assets/img/lupin.jpg",
+            description: "short description .",
+            // facebook: "https://facebook-url.com",
+            // linkedin: "https://linkedin-url.com",
+            // github: "https://github.com/OmerHassin"
+        },
+        {
+            name: "Alon Lavi",
+            // image: "./assets/img/kfir.png",
+            description: "short description ",
+            // facebook: "https://facebook-url.com",
+            // linkedin: "https://linkedin-url.com",
+            // github: "https://github.com/kfirshaked45"
+        }
+    ];
+
+    return (
+        <section className="about">
+            <h1>About Page</h1>
+            {teamMembers.map((member, index) => (
+                <div key={index} className="team-member">
+                    <img src={member.image} alt={member.name} className="profile-picture" />
+                    <h2>{member.name}</h2>
+                    <p>{member.description}</p>
+                    <div className="social-media-links">
+                        <a href={member.facebook} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-facebook fa-2xl"></i></a>
+                        <a href={member.linkedin} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin fa-2xl"></i></a>
+                        <a href={member.github} target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github fa-2xl"></i></a>
+                    </div>
+                </div>
+            ))}
+        </section>
+    )
+  }
